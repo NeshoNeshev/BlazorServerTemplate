dcab57c53ac8dc02cc384d3e60284c8e1e828657
NeshoNeshev <69240150+NeshoNeshev@users.noreply.github.com>
2022-08-18 16:03:32 +0300

    Add project files.

---
+++ Data/BlazorServerTemplate.Data/ApplicationDbContext .cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorServerTemplate.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+
+        //Database Models
+    }
+}
